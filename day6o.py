8583f08ff5ad14188e127ad816c795390afb1d62
JYOTHSNA RANI <121158628+Jyot853@users.noreply.github.com>
2022-12-27 17:15:42 +0530

    Add files via upload

---
+++ day6o.py
@@ -0,0 +1,13 @@
+string='jyothsna is very good and multi talent girl you konw that'
+ans=[i for i in string if i not in 'AEIOUaeiou' ]
+print('*'.join(ans))
+
+output:
+
+jythsn s vry gd nd mlt tlnt grl y knw tht
+
+==================== ===============
+j*y*t*h*s*n* *s* *v*r*y* *g*d* *n*d* *m*l*t* *t*l*n*t* *g*r*l* *y* *k*n*w* *t*h*t
+
+==================== ===
+j*y*t*h*s*n* *s* *v*r*y* *g*d* *n*d* *m*l*t* *t*l*n*t* *g*r*l* *y* *k*n*w* *t*h*t
